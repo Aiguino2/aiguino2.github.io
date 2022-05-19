accc0f9386780002dd2644642f88ca88845e9ce2
Aiguino2 <105807362+Aiguino2@users.noreply.github.com>
2022-05-19 16:43:17 +0200

    Actualización uwu

--- js.js
+++ js.js
@@ -1,8 +1,35 @@
 import enlaces from "./json/url.json" assert {type:"json"};
-
+var articulos = [];
 window.onload = function() {
     for (let i=0;i<enlaces.length;i++) {
         document.body.innerHTML+= 
-        `<article><h1>${enlaces[i].nombre}</h1></article>`
+        `<article id="${i+1}"><h1>${enlaces[i].nombre}</h1></article>`
+    }
+
+    articulos = articulos.slice.call(document.querySelectorAll("article"));
+
+    console.log(articulos);
+
+    articulos.forEach(art => {
+        art.addEventListener("mouseover", hover);
+        art.addEventListener("mouseout",noHover);
+        art.addEventListener("click",function() {
+            window.open(enlaces[(art.id)-1].enlace);
+            window.close();
+        });
+    });
+
+    function hover(e) {
+        articulos.forEach(art => {
+            if (art != e.target) {
+                art.classList.add("blur");
+            }
+        });
+    }
+
+    function noHover() {
+        articulos.forEach(art =>{
+            art.classList.remove("blur");
+        });
     }
 }
