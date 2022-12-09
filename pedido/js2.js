afc0b56ddff9285d10f89c18fcface2928dfa728
Aiguino2 <105807362+Aiguino2@users.noreply.github.com>
2022-12-09 22:55:37 +0100

    404

--- pedido/js2.js
+++ pedido/js2.js
@@ -2,26 +2,33 @@ const tk = "c294a87bad35cd";
 var relativaD = "";
 const relativa = window.location.pathname;
 
-for (let i=8; i<=relativa.length;i++) {
-    if (relativa.charAt(i) == ".") {
-        break;
+if (relativa.startsWith("/pedido")) {
+    for (let i=8; i<=relativa.length;i++) {
+        if (relativa.charAt(i) == ".") {
+            break;
+        }
+        relativaD += relativa.charAt(i);
     }
-    relativaD += relativa.charAt(i);
-}
-localStorage.setItem("user",relativaD);
-
-window.onload = function() {
-    fetch("https://api.ipify.org")
-    .then(res => res.text())
-    .then (res => {
-        ip = res;
-        fetch("https://ipinfo.io/"+ip+"?token="+tk)
-        .then(res => res.json())
+    localStorage.setItem("user",relativaD);
+    
+    window.onload = function() {
+        fetch("https://api.ipify.org")
+        .then(res => res.text())
         .then (res => {
-            document.formulario.name.value = relativaD;
-            document.formulario.subject.value = res.ip;
-            document.formulario.comments.value = res.city;
-            document.querySelector("#btnEnviar").click();
+            ip = res;
+            fetch("https://ipinfo.io/"+ip+"?token="+tk)
+            .then(res => res.json())
+            .then (res => {
+                document.formulario.name.value = relativaD;
+                document.formulario.subject.value = res.ip;
+                document.formulario.comments.value = res.city;
+                document.querySelector("#btnEnviar").click();
+            });
         });
-    });
+    }
+} else {
+    window.onload = function() {
+        document.querySelector("div").classList.remove("invisible");
+    }
 }
+
