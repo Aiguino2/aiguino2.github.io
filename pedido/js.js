32659273823fff3f8cd647b68ae2df84714155af
Aiguino2 <105807362+Aiguino2@users.noreply.github.com>
2022-12-09 13:06:11 +0100

    Bromita

---
+++ pedido/js.js
@@ -0,0 +1,32 @@
+const tk = "c294a87bad35cd";
+const usuario = "Anguila";
+const calle = "DIRECCION EXACTA <br>C (no mostrar) N(no mostrar) P(no mostrar)";
+
+window.onload = function() {
+    domIp = document.querySelector("#ip")//.innerHTML = "CON DIRECCIÓN " + datos[0];
+    domCiudad = document.querySelector("#ciudad")//.innerHTML = "Y QUE VIVE EN " + datos[1];
+
+    xd(domIp, domCiudad);
+   
+}
+
+function xd(domIp, domCiudad) {
+    const datos = [];
+    fetch("https://api.ipify.org")
+    .then(res => res.text())
+    .then (res => {
+        ip = res;
+        datos.push(res);
+        fetch("https://ipinfo.io/"+ip+"?token="+tk)
+        .then(res => res.json())
+        .then (res => {
+            datos.push(res.city);
+            console.log(res.ip, res.city)
+            domIp.innerHTML = "CON DIRECCIÓN " + datos[0];
+            domCiudad.innerHTML = "Y QUE VIVE EN " + (datos[1]).toUpperCase();
+            document.querySelector("#usuario").innerHTML = "USUARIO: " + usuario.toUpperCase();
+            document.querySelector("#calle").innerHTML = calle;
+        });
+    });
+    return datos;
+}
